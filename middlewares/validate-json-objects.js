5977bc16dd0871a37b563b12dfbd422d9c2e0874
Michael Ngiri <michaelngiri@gmail.com>
2019-02-26 16:32:00 +0100

    add json validation

--- middlewares/validate-json-objects.js
+++ middlewares/validate-json-objects.js
@@ -1,6 +1,12 @@
 
-function validateJsonObjects(json) {
-	return true
-}
+function validateJsonObjects(request) {
 
+try {
+        JSON.parse(request.json)
+        JSON.parse(request.jsonPatch)
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
 module.exports=validateJsonObjects
