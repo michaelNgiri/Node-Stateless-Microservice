436a0b561a180df726681f211c14f31f1dee756d
Michael Ngiri <michaelngiri@gmail.com>
2019-02-26 11:10:50 +0100

    middleware for authentication

---
+++ middlewares/verify-user.js
@@ -0,0 +1,16 @@
+ /// <summary>
+ /// Verifies that the user information is in a valid format and not empty
+ /// </summary>
+ /// <param name="user"></param>
+ /// <returns>true for a valid user and false for an invallid user</returns>
+function loginIsValid(user){
+    if (typeof user.username === "string" &&
+        user.username.trim() !== '' &&
+        typeof user.password === "string" &&
+        user.password.trim() !== '') {
+        return true;
+    }
+    console.log(user);
+    return false;
+}
+module.exports = loginIsValid;
