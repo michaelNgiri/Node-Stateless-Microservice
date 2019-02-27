0ca4abf78e506085dd3d47123efea1766f761bb8
Michael Ngiri <michaelngiri@gmail.com>
2019-02-27 06:19:04 +0100

    download image and save thumbnails

--- controllers/ThumbnailDownloadController.js
+++ controllers/ThumbnailDownloadController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const sharp = require('sharp')
 const download = require('image-downloader')
+const getFileExtension = require('../helpers/FileExtensionHelper')
 
 
 const mimes = ['jpg', 'jpeg', 'rtf', 'tif', 'gif', 'png', 'svg']
@@ -10,23 +11,34 @@ const imageDownloadsFolder = 'public/img/downloads'
 const thumbnailsFolder = 'public/img/thumbnails'
 
 
-// const { fileExtension } = (url) => { return url.split('.').pop().split(/\#|\?/)[0] }
-
-// Resize image on post.
-//route to patch json
+//route to generate thumbnails
 router.post('/', (req, res, next)=>{
- 
+const ImageName = getFileExtension(req.body.imageLink)
+
 const options = {
   url: req.body.imageLink,
-  dest: thumbnailsFolder                
+  dest: imageDownloadsFolder               
 }
+
 download.image(options)
-  .then(({ filename, image }) => {
-    console.log('File saved to', filename)
-  })
-  .catch((err) => {
-    console.error(err)
-  })
+   .then(({ filename }) => {
+        // Resize image to 50x50 and save to desired location.
+        // Return conversion status to user.
+        sharp(filename)
+          .resize(50, 50)
+          .toFile(thumbnailsFolder+'/'+ImageName, (err) => {
+            if (err) { return next(err) }
+            return res.status(200).json({
+              status: 200+' success',
+              thumbnail: thumbnailsFolder,
+            })
+          })
+      })
+      .catch(() => {
+        res.status(500).json({ 
+
+          error: 'Could not generate thumbnail, check the link you pasted or try again later' })
+      })
  
 })
 
