c1e110491dc0debd63268b2d868ad1cef9f84704
Michael Ngiri <michaelngiri@gmail.com>
2019-02-27 06:37:24 +0100

    send image thumbnail to user

--- controllers/ThumbnailDownloadController.js
+++ controllers/ThumbnailDownloadController.js
@@ -4,8 +4,7 @@ const sharp = require('sharp')
 const download = require('image-downloader')
 const getFileExtension = require('../helpers/FileExtensionHelper')
 
-
-const mimes = ['jpg', 'jpeg', 'rtf', 'tif', 'gif', 'png', 'svg']
+const fs = require('fs')
 
 const imageDownloadsFolder = 'public/img/downloads'
 const thumbnailsFolder = 'public/img/thumbnails'
@@ -15,10 +14,10 @@ const thumbnailsFolder = 'public/img/thumbnails'
 router.post('/', (req, res, next)=>{
 const ImageName = getFileExtension(req.body.imageLink)
 
-const options = {
-  url: req.body.imageLink,
-  dest: imageDownloadsFolder               
-}
+  const options = {
+    url: req.body.imageLink,
+    dest: imageDownloadsFolder               
+  }
 
 download.image(options)
    .then(({ filename }) => {
@@ -28,20 +27,24 @@ download.image(options)
           .resize(50, 50)
           .toFile(thumbnailsFolder+'/'+ImageName, (err) => {
             if (err) { return next(err) }
-            return res.status(200).json({
-              status: 200+' success',
-              thumbnail: thumbnailsFolder,
-            })
+
+            //return the generated thumbnail
+            fs.readFile(thumbnailsFolder+'/'+ImageName, function(err, data) {
+              if (err) throw err; // Fail if the file can't be read.
+             
+                //set the response headers
+                res.writeHead(200, {'Content-Type': 'image/jpeg'});
+                // Send the file data to the cliemt.
+                res.end(data); 
+               })
           })
-      })
-      .catch(() => {
-        res.status(500).json({ 
+          .catch(() => {
+            res.status(500).json({ 
 
-          error: 'Could not generate thumbnail, check the link you pasted or try again later' })
-      })
- 
+              error: 'Could not generate thumbnail, check the link you pasted or try again later' })
+          })
+     })
 })
-
 
 module.exports=router
 
